beb741af117cdbe93a562a4976a50dfea94a4183
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-06-27 22:51:41 -0300

    Add exercises 006-007 on C++ new lines
    Add two new C++ exercises demonstrating different methods of inserting new lines in output using escape sequences (\n) and the endl manipulator. Includes accompanying notes explaining escape sequences and their usage.

---
+++ 1-CPP_Tutorial/exercises/ex006.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World! \n";
+	cout << "I am learning C++" << "\n";
+	cout << "I am eager to delve deeper into the teachings." << "\n\n";
+	cout << "See you later!";
+	return 0;
+}
