5d3827946d876d509706cb7871d816a3fd36ba84
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-07-02 16:38:18 -0300

    Add exercise ex008 and notes; update project files
    Add exercises/ex008.cpp (example showing single- and multi-line comments) and notes/ex008.md documenting C++ comments. Update W3Schools.cpp.notes.vcxproj to include exercises\ex008.cpp and notes\ex008.md, and set challenges\C002Part2.cpp to be excluded from Debug|x64 build. Update W3Schools.cpp.notes.vcxproj.filters to place the new files in the correct filters.

---
+++ 1-CPP_Tutorial/exercises/ex008.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	/* The code below will print the words Hello World!
+	to the screen, and it is amazing */
+	// This is a comment
+	cout << "Hello World!"; // This is a comment
+	return 0;
+}
