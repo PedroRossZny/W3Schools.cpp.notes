e12ddde12093389f26450d5ce687b17b41a0e16e
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-07-02 19:56:24 -0300

    Add exercises ex009-ex012 on C++ variables
    Add four new exercises and documentation covering C++ variables. Exercises demonstrate variable declaration, data type usage (int, double, char, string, bool), output with cout, and basic arithmetic operations. Update project files to include new source files.

---
+++ 1-CPP_Tutorial/exercises/ex009.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int myNum = 15;
+	cout << myNum;
+
+	int secondNum;
+	secondNum = 20;
+	cout << secondNum;
+
+	myNum = 10;
+	cout << myNum;
+
+	return 0;
+}
---
+++ 1-CPP_Tutorial/exercises/ex011.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int myAge = 35;
+	cout << "I am " << myAge << " years old.\n";
+
+	string name = "John";
+	int age = 35;
+	double height = 6.1;
+	cout << name << " is " << age << " years old and " << height << " feet tall.";
+
+	return 0;
+}
