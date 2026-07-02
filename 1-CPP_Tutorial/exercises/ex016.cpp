67fdacd0113301e44150c003380c2dc019072e77
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-07-02 20:44:16 -0300

    Add exercises ex016-ex017 for C++ constants
    Add two new exercises demonstrating const keyword usage in C++:
    
    - ex016: Shows how to declare variables as constants using const keyword
    - ex017: Demonstrates that constants must be initialized at declaration time
    
    Includes markdown documentation explaining the concepts and updates to project files.

---
+++ 1-CPP_Tutorial/exercises/ex016.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int myNum = 15; // myNum will always be 15
+	// myNum = 10; // error: assigment of read-only variable 'myNum'
+	const int minutesPerHour = 60;
+
+	return 0;
+}
---
+++ 1-CPP_Tutorial/exercises/ex017.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const int minutesPerHour = 60;
+
+	/*const int minutesPerHour;
+	minutesPerHour = 60;*/ // error
+
+	return 0;
+}
