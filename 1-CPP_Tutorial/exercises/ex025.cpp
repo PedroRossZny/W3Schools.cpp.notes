c0f7e44656e78edbdbd2b0a44d0ba2b7d067b234
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-07-09 03:59:29 -0300

    Add exercise 025: Character data types
    Add exercise demonstrating C++ char type and ASCII values. Includes ex025.cpp program showing character storage and ASCII value usage, along with corresponding documentation.

---
+++ 1-CPP_Tutorial/exercises/ex025.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char myGrade = 'B';
+
+	cout << myGrade;
+
+	char a = 65, b = 66, c = 67;
+
+	cout << a;
+	cout << b;
+	cout << c;
+
+	return 0;
+}
