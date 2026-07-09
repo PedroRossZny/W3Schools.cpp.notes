be90a4ba5994fc0ff6e01ecad2106c2baa27584d
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-07-09 03:40:32 -0300

    Add exercise 023: C++ numeric types
    Add exercise demonstrating int, float, and double types, including scientific notation. Includes documentation covering numeric type usage and precision differences between float and double. Update project files to include the new exercise.

---
+++ 1-CPP_Tutorial/exercises/ex023.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int myIntNum = 1000;
+	cout << myIntNum;
+
+	float myFloatNum = 5.75;
+	cout << myFloatNum;
+
+	double myDoubleNum = 19.99;
+	cout << myDoubleNum;
+
+	float f1 = 35e3;
+	double d1 = 12E4;
+
+	cout << f1;
+	cout << d1;
+
+	return 0;
+}
