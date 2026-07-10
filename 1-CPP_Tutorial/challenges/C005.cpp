b56af78766ea73d553f2202c6f309fe4517a8721
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-07-10 01:48:58 -0300

    Add Challenge 005 and statement
    Add two challenge sources (challenges/C005.cpp, challenges/C005Part2.cpp) and the challenge description (statements/005.md). Update W3Schools.cpp.notes.vcxproj and .vcxproj.filters to include the new files and adjust build settings (C005.cpp and exercises/ex042.cpp set as ExcludedFromBuild for Debug|x64).

---
+++ 1-CPP_Tutorial/challenges/C005.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int itemPrice = 52;
+	int shippingCost = 8;
+
+	int sum = itemPrice + shippingCost;
+
+	cout << "Total price: " << sum << "$";
+
+	return 0;
+}
---
+++ 1-CPP_Tutorial/challenges/C005Part2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int itemPrice = 50;
+	int shippingCost = 15;
+
+	int sum = itemPrice + shippingCost;
+
+	cout << sum;
+
+	return 0;
+}
