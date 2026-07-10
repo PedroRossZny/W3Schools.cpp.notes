5fe923df320b5ec6d4a8ef559ac6cbc0a2fcae4f
Pedro Henrique Rodrigues Ross <pedrohenriquerodriguesross@gmail.com>
2026-07-10 00:07:58 -0300

    Add exercise 040 (logical operators)
    Add new exercise source 1-CPP_Tutorial/exercises/ex040.cpp demonstrating logical operators and notes 1-CPP_Tutorial/notes/ex040.md. Update 1-CPP_Tutorial/W3Schools.cpp.notes.vcxproj and .filters to include the new files. Also set exercises/ex039.cpp ExcludedFromBuild to true for Debug|x64.

---
+++ 1-CPP_Tutorial/exercises/ex040.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 5;
+	cout << (x > 3 && x < 10) << "\n"; // returns true (1) because 5 is greater than 3 AND 5 is less than 10
+	cout << (x > 3 || x < 4) << "\n"; // returns true (1) because one of the conditions are true (5 is greater than 3, but 5 is not less than 4)
+	cout << (!(x > 3 && x < 10)) << "\n"; // returns false (0) because ! (not) is used to reverse the result
+
+	return 0;
+}
